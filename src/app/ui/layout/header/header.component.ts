8ca9e1365596eaea6c96c126de345c5dc9ec144c
Ernesto Tenorio <etenorio87@gmail.com>
2022-04-22 14:35:04 +0200

    Ocultando elementos cuando el usuario esta logueado

--- src/app/ui/layout/header/header.component.ts
+++ src/app/ui/layout/header/header.component.ts
@@ -1,5 +1,7 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { MenuItem, PrimeIcons } from 'primeng/api';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -10,6 +12,8 @@ export class HeaderComponent implements OnInit {
 
   menuItems: MenuItem[] = [];
 
+  constructor(public auth: AuthService,
+              private router: Router ) { }
 
   ngOnInit(): void {
 
@@ -26,6 +30,11 @@ export class HeaderComponent implements OnInit {
         }
     ];
 
+  }
+
+  disconnect(): void {
+    this.auth.disconnect();
+    this.router.navigate(['/login']);
   }
 
 }
--- src/app/ui/layout/layout.module.ts
+++ src/app/ui/layout/layout.module.ts
@@ -1,16 +1,20 @@
+import { ButtonModule } from 'primeng/button';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenubarModule } from 'primeng/menubar';
 
 import { HeaderComponent } from './header/header.component';
+import { SharedModule } from 'primeng/api';
 
 
 
 @NgModule({
   declarations: [HeaderComponent],
   imports: [
+    ButtonModule,
     CommonModule,
-    MenubarModule
+    MenubarModule,
+    SharedModule
   ],
   exports: [HeaderComponent]
 })
